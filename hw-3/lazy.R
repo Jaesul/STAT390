96092bea966f420853504be648e28417cbb4f806
jaesuk <jaesuklee@live.com>
2020-02-11 00:17:08 -0800

    finished

--- hw-3/lazy.R
+++ hw-3/lazy.R
@@ -11,6 +11,8 @@ abline(lm.1)
 #R^2 = 0.4183 the variance of amount of money spend of week is due to the drinks per week 
 #Se = 6.944 Typical error from the from the fit line is 6.944
 
+
+par(mfrow=c(1,1)) 
 transform_dat <- read.csv("C:/Users/jaesu/Downloads/transform_dat.txt", sep="")
 x <- transform_dat$x
 y <- transform_dat$y
@@ -20,10 +22,12 @@ plot(x,y)
 xs <- sqrt(x)
 ys <- sqrt(y)
 
-plot(x,y)
+plot(xs,ys)
 
 lm.1 <- lm(ys ~xs)
 summary(lm.1)
+lines(lm.1$fitted.values)
+abline(lm.1)
 
 # 99% of transformed y is due to transformed and the typical error is 0.019
 
@@ -121,6 +125,8 @@ summary(lm.12)
 # The model used should be ydat2 ~ x2dat2 + I(x2dat2^2) there is colinearity between x1 and x2 which means 
 # that one is unnecessary and should be removed, I chose x1 to be removed because R^2 is better for that one 
 
+
+par(mfrow=c(2,2)) 
 mult <- read.table("C:/Users/jaesu/Downloads/hw_3_mult_simple_dat.txt", quote="\"", comment.char="")
 
 x1m <- mult$V1
@@ -157,24 +163,22 @@ y.fit
 library(lattice)
 cloud(y.fit, type="p")
 
+
 par(mfrow=c(1,2)) 
 
 ntrial = 5000 
 xmax = numeric(ntrial)
 for(trial in 1:ntrial) {
   x = rnorm(50, 0, 1)
-  hist(x, breaks=10)
   xmax[trial] = max(x)
 }
 
-xmax
 
 hist(xmax, main="")
 
 xmin = numeric(ntrial)
 for(trial in 1:ntrial) {
   x = rnorm(50, 0, 1)
-  hist(x, breaks=10)
   xmin[trial] = min(x)
 }
 hist(xmin, main="")
@@ -187,16 +191,50 @@ ntrial = 5000
 xmean = numeric(ntrial)
 for(trial in 1:ntrial) {
   x = rexp(100, 2)
-  hist(x, breaks=10)
   xmean[trial] = mean(x)
 }
-xmean
 hist(xmean, main="")
+
+qqnorm(xmean, cex=0.5)
 
 mean(xmean)
 sd(xmean)
-sqrt(.007)
-.5/10
+.5/sqrt(5000)
 
-qqnorm(xmean, cex=0.5)
+# The means are close but the predicted deviation is not close to the actual 
+# actual = 0.07 predicted = 0.05
+
+
+
+
+
+
+
+par(mfrow=c(2,2)) 
+hw4.book.1 <- read.csv("~/hw4-book-1.txt", sep="")
+
+x <- hw4.book.1$Cycfail
+y <- hw4.book.1$Strampl
+
+plot(x,y)
+plot(log(x),y)
+plot(log(x),log(y))
+plot(1/x,1/y)
+
+summary(lm(y ~ log(x)))
+
+0.0197092 + -0.0012805*log(5000)
+
+
+thickness <- c(220,220,220,220,370,370,370,370,440,440,440,440,680,680,680,680,860,860,860,860)
+str <- c(24.0,22.0,19.1,15.5,26.3,24.6,23.1,21.2,25.2,24.0,21.7,19.2,17.0,14.9,13.0,11.8,12.2,11.2,6.6,2.8)
+plot(thickness, str)
+lm.6 <- lm(str ~ thickness + I(thickness^2))
+lines(thickness, lm.6$fitted.values)
+summary(lm(str ~ thickness + I(thickness^2)))
+
+1.452e+01 + 4.323e-02*500 -6.001e-05 * 500^2
+
+.3/6
+
 
